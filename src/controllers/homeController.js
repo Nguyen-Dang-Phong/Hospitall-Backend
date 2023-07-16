6ea88c152c030f06c072331fc872329edb7fabbd
Nguyen-Dang-Phong <nguyendangphong0099@gmail.com>
2023-07-17 03:07:53 +0700

    Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View
    #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,16 @@
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+import dt from '../models/index'
+let getHomePage = async (req, res) => {
+    try {
+        let data = await dt.User.findAll()
+        console.log(data)
+        return res.render('homepage.ejs', {
+            // truyen data ra view
+            data: JSON.stringify(data)
+        });
+
+    } catch (e) {
+        console.log(e)
+    }
 }
 let getAbout = (req, res) => {
     return res.render('test/about.ejs');
