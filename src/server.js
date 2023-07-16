0da92a98e00bd87b3a67520139f5c513fa11946f
Nguyen-Dang-Phong <nguyendangphong0099@gmail.com>
2023-07-16 22:23:14 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine"
+import initWebRouter from "./route/web"
+require('dotenv').config();
+
+let app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+viewEngine(app)
+initWebRouter(app)
+let port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log("Backend Nodejs")
+})
