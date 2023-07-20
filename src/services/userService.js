2315edc69d5600dc6e5f49d09a65e9cd76764ecd
Nguyen-Dang-Phong <nguyendangphong0099@gmail.com>
2023-07-20 17:18:43 +0700

    CRUD với ReactJS Phần 1 - Viết API Lấy Tất Cả Thông Tin Người Dùng

--- src/services/userService.js
+++ src/services/userService.js
@@ -56,6 +56,34 @@ let checkUserEmail = (userEmail) => {
         }
     })
 }
+let getAllUsers = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = ''
+            if (userId === 'ALL') {
+                users = await dt.User.findAll({
+                    attributes: {
+                        exclude: ['password']
+                    }
+                })
+            }
+
+
+            if (userId && userId !== 'ALL') {
+                users = await dt.User.findOne({
+                    where: { id: userId },
+                    attributes: {
+                        exclude: ['password']
+                    }
+                })
+            }
+            resolve(users)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 module.exports = {
-    handleUserLogin: handleUserLogin
+    handleUserLogin: handleUserLogin,
+    getAllUsers: getAllUsers
 }
