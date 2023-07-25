163d16d078424f85f77f1415c0be9ffc8f31b6f1
Nguyen-Dang-Phong <nguyendangphong0099@gmail.com>
2023-07-25 14:12:58 +0700

    CRUD React 4-5 - Thêm Mới Người Dùng (CREATE)

--- src/services/userService.js
+++ src/services/userService.js
@@ -105,23 +105,24 @@ let createNewUser = (data) => {
                     errCode: 1,
                     errMessage: "The Email is succesfull"
                 })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await dt.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender == '1' ? true : false,
+                    roleId: data.roleId,
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: "OK"
+                })
             }
-            console.log(data.password)
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await dt.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender == '1' ? true : false,
-                roleId: data.roleId,
-            })
-            resolve({
-                errCode: 0,
-                errMessage: "OK"
-            })
+
         } catch (error) {
             reject(error)
         }
